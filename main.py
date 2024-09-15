7e58a18c556c66433f5a1d9c8e10d7f94a71b300
zomb37 <howlingnights@gmail.com>
2024-09-15 15:58:04 -0700

    added movement functionality to the triangle/main player character

--- main.py
+++ main.py
@@ -3,16 +3,24 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
 	pygame.init()
+	newPlayer = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	newClock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0,0,0))
+		newPlayer.draw(screen)
+		newPlayer.update(dt)
 		pygame.display.flip()
+		dt = newClock.tick(60) / 1000
+
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
---
+++ player.py
@@ -0,0 +1,40 @@
+from constants import *
+from circleshape import *
+
+
+class Player(CircleShape):
+	def __init__(self, x, y):
+		super().__init__(x, y, PLAYER_RADIUS)
+		self.rotation = 0
+
+	# in the player class
+	def triangle(self):
+		forward = pygame.Vector2(0, 1).rotate(self.rotation)
+		right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+		a = self.position + forward * self.radius
+		b = self.position - forward * self.radius - right
+		c = self.position - forward * self.radius + right
+		return [a, b, c]
+
+	def draw(self, screen):
+		pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+	def rotate(self, delta_time):
+		self.rotation += PLAYER_TURN_SPEED * delta_time
+
+	def move(self, dt_input):
+                forward_movement = pygame.Vector2(0, 1).rotate(self.rotation)
+                self.position += forward_movement * PLAYER_SPEED * dt_input
+
+	def update(self, new_dt):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_a]:
+			self.rotate(-new_dt)
+		if keys[pygame.K_d]:
+			self.rotate(new_dt)
+		if keys[pygame.K_w]:
+			self.move(new_dt)
+		if keys[pygame.K_s]:
+			self.move(-new_dt)
+
